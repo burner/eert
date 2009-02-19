53717b9e5413ec0f8555fdfd35f61f94a5a5248c
Robert BuRnEr Schadek <burner@burner-desktop.(none)>
2009-02-19 21:39:43 +0100

    webstart is a son of bitch very very stupid to force us using this

--- Content/Szenes/Szenes.java
+++ Content/Szenes/Szenes.java
@@ -2,38 +2,21 @@
  *EERT = EERT enhanced rendering technology
  *
  *Copyright (C) [2008] [Robert "BuRnEr" Schadek]
+
  *This program is free software; you can redistribute it and/or modify it under
  *the terms of the GNU General Public License as published by the Free Software
  *Foundation; either version 3 of the License,
  *or (at your option) any later version.
+
  *This program is distributed in the hope that it will be useful, but WITHOUT
  *ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
  *FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+
  *You should have received a copy of the GNU General Public License along with
  *this program; if not, see <http://www.gnu.org/licenses/>.
  */
+package Content.Szenes;
 
-import Util.Logic.EFrame;
-import Util.*;
 
-public class Eert {
-
-    private static EFrame frame;
-/*
-    public static void main(String[] args) {
-        String szene = args[0];
-        String fullscreen;
-        boolean boolScreen = false;
-        if (args.length == 2) {
-            fullscreen = args[1];
-            if (fullscreen.contains("Fullscreen")) {
-                boolScreen = true;
-            }
-        }
-     
-        frame = new EFrame(szene, boolScreen);
-        Eert.frame.animator.start();
-        System.out.println("Before leaving main");
-    }
- */
+public class Szenes {
 }
